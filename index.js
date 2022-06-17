1f9ae67966d9b18a4d4027b5acddb4e9b0e6f45e
Gospel Chinyereugo <gospelokpara@gmail.com>
2022-06-17 19:10:17 +0100

    --feat: beta version 1

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = function () {
-    console.log("Hello Mars!!");
+module.exports = function (lang) {
+    if (lang === "es") {
+        console.log("Hola Marte!!");
+    } else {
+        console.log("Hello Mars!!");
+    }
 }
